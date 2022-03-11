48b7ed8efa04327c091cbb7668a2675f0f482c9d
FelippeAlves <felippealves17@gmail.com>
2022-03-11 19:15:31 -0300

    Finish footer

---
+++ src/components/FooterComponent/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './styles.css';
+
+function FooterComponent() {
+    return <>
+        <footer>
+            <div className="footerContainer">
+                <p>@Newtab Academy 2021. Todos os direitos reservados</p>
+            </div>
+        </footer>
+    </>
+}
+
+export default FooterComponent;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HeaderComponent from './components/HeaderComponent'
+import FooterComponent from './components/FooterComponent'
 
 ReactDOM.render(
   <React.StrictMode>
     <HeaderComponent />
+    <FooterComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
