821beae863df2f32feace4cdcc38a358477f1687
FelippeAlves <felippealves17@gmail.com>
2022-03-12 21:40:13 -0300

    Finish cards component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import HeaderComponent from './components/HeaderComponent'
 import FooterComponent from './components/FooterComponent'
 import CarouselComponent from './components/CarouselComponent'
+import PostComponent from './components/PostComponent'
+
 
 ReactDOM.render(
   <React.StrictMode>
     <HeaderComponent />
-      <CarouselComponent />
+      <PostComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
