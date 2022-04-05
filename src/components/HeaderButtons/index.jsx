90f64661eeacd61562eb4404fe2186a6366cb753
Pedro Lima <phlimas@outlook.com>
2022-04-05 13:29:10 -0300

    Renvido fluxo dos btn da header

--- src/components/HeaderButtons/index.jsx
+++ src/components/HeaderButtons/index.jsx
@@ -4,55 +4,74 @@ import "./styles.css";
 import iconAbout from "../../assets/icon-info-circle.svg";
 import iconLogin from "../../assets/icon-user-alt.svg";
 import iconPower from "../../assets/icon-power-off.svg";
+import iconHome from "../../assets/icon-home.svg";
 
 import { useAuth } from "../../context/AuthContext";
 
 function HeaderButtons() {
   const [showLoginBtn, setShowLoginBtn] = useState(true);
+  const [locationNow, setLocationNow] = useState(false);
   const { isAuthenticate, setIsAuthenticate } = useAuth();
   const location = useLocation();
 
   useEffect(() => {
     if (location.pathname === "/login") {
       setShowLoginBtn(false);
-    } else {
-      setShowLoginBtn(true);
+    }
+    if (location.pathname === "/search-listing") {
+      setLocationNow(true);
     }
   }, [location]);
 
-
-
-  const [active, setActive] = useState(false)
+  const [active, setActive] = useState(false);
 
   const verify = () => {
     const scroll = window.scrollY;
     const height = window.innerHeight;
-    if(scroll > (height/1.55)) {
+    if (scroll > height / 1.55) {
       setActive(true);
     } else {
       setActive(false);
     }
-  }
+  };
 
-  window.addEventListener('scroll', verify)
+  window.addEventListener("scroll", verify);
 
   return (
     <>
-      <div className={active ? 'container active' : 'container'}>
+      <div className={active ? "container active" : "container"}>
         <Link to="/" className="sloganContainer">
-          <p className={active ? 'pink' : ''}>hashtag</p>
-          <p className={active ? 'sloganFinder pink' : 'sloganFinder'}>finder</p>
+          <p className={active ? "pink" : ""}>hashtag</p>
+          <p className={active ? "sloganFinder pink" : "sloganFinder"}>
+            finder
+          </p>
         </Link>
         <nav className={`${showLoginBtn && "btnContainer"}`}>
           <Link
-            to={isAuthenticate || !showLoginBtn ? "/search-listing" : "/sobre"}
+            to={
+              isAuthenticate && !locationNow
+                ? "/search-listing"
+                : !showLoginBtn || locationNow
+                ? "/"
+                : "/sobre"
+            }
             className="btnAbout"
           >
-            <img className="icons" src={iconAbout} alt="" />
+            <img
+              className="icons"
+              src={!showLoginBtn || locationNow ? iconHome : iconAbout}
+              alt=""
+            />
+
             <p className="btnLetter">
-              {isAuthenticate || !showLoginBtn ? "BUSCAS" : "SOBRE"}
+              {isAuthenticate && !locationNow
+                ? "BUSCAS"
+                : !showLoginBtn || locationNow
+                ? "HOME"
+                : "SOBRE"}
             </p>
           </Link>
+
           {showLoginBtn && (
             <Link to={isAuthenticate ? "" : "/login"}>
               <div
