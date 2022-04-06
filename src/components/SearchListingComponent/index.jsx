db8617dacabe149ae6addd9c37d70b4a66b9018b
FelippeAlves <felippealves17@gmail.com>
2022-04-06 00:39:39 -0300

    adding loader on search listing

--- src/components/SearchListingComponent/index.jsx
+++ src/components/SearchListingComponent/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import './styles.css';
 import HeaderButtons from "../HeaderButtons";
+import HashLoader from "react-spinners/HashLoader";
 
 
 function SearchListingComponent() {
@@ -28,6 +29,7 @@ function SearchListingComponent() {
             setDataLocal(result.records.reverse())
             setPages(Math.ceil(result.records.length / 10))         
         }
+        setTimeout(() => {setLoading(false)}, 800);
 
         getSearchs()
     }, [])
@@ -36,6 +38,7 @@ function SearchListingComponent() {
     const startIndex = currentPage * 10;
     const endIndex = startIndex + 10;
     const [pages, setPages] = useState(10)
+    const [loading, setLoading] = useState(true)
     const currentItens = dataLocal.slice(startIndex, endIndex)
 
     const item = currentItens.map((item) => {
@@ -54,28 +57,38 @@ function SearchListingComponent() {
     }) 
 
     return <>
+
+
         <div className="searchListingContainer">
             <HeaderButtons/>
-            <div className="table">
-                <h2>Buscas realizadas</h2>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Hashtag</th>
-                            <th>Data</th>
-                            <th>Hora</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {item}
-                    </tbody>
-                </table> 
-                <ul>
-                    <li>{Array.from(Array(pages), (item, index) => {
-                        return <button className="pageButtons" value={index} onClick={(e) => setCurrentPage(Number(e.target.value))}>{index +1}</button>
-                    })}</li>
-                </ul>
-            </div>
+            {
+                loading ? 
+                
+                <div className="loaderContainer">
+                    <HashLoader color="#fff" />
+                    </div>
+                    :
+                <div className="table">
+                    <h2>Buscas realizadas</h2>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Hashtag</th>
+                                <th>Data</th>
+                                <th>Hora</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {item}
+                        </tbody>
+                    </table> 
+                    <ul>
+                        <li>{Array.from(Array(pages), (item, index) => {
+                            return <button className="pageButtons" value={index} onClick={(e) => setCurrentPage(Number(e.target.value))}>{index +1}</button>
+                        })}</li>
+                    </ul>
+                </div>
+            }
         </div>
     </>
 }
